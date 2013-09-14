cec7fb2d4353d385e70ebe21a2e5d11cc9a3353c
Luke Humphreys <lhumphreys67@gmail.com>
2013-09-14 17:53:37 +0100

    Use HTTPS to pull libDefer
    By pulling by HTTPS we allow someone cloning the depot to also cone the
    dependencies

--- Scripts/TRAVIS.sh
+++ Scripts/TRAVIS.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-#
-# Make sure DEV_TOOLS's submodules have been intialised
-#
-
-git submodule update --init --recursive
-
-#
-# Install GNU c++11 compiler
-#
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update -qq
-sudo apt-get install -qq g++-4.8;
-sudo apt-get install -qq gfortran-4.8
-
 # Build with the TRAVIS tool chain
 TOOL_CHAIN=TRAVIS PROJECT_ROOT_DIR=$PWD make $*
