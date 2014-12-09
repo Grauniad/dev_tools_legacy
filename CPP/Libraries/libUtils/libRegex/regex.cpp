4fc9c5f0d00382547f13dc10e67ef098111476c3
Luke.Humphreys <lhumphreys67@gmail.com>
2014-12-09 23:19:07 +0000

    Fix regex to work without boost

--- CPP/Libraries/libUtils/libRegex/regex.cpp
+++ CPP/Libraries/libUtils/libRegex/regex.cpp
@@ -1,33 +1,33 @@
 #include "regex.h"
-#include <boost/regex.hpp>
+#include <regex>
 
 using namespace std;
 RegPattern::RegPattern (const string& needle)
     : pattern(NULL)
 {
     try {
-        pattern = reinterpret_cast<void *>(new boost::regex(needle));
-    } catch ( boost::regex_error& e ) {
+        pattern = reinterpret_cast<void *>(new regex(needle));
+    } catch ( regex_error& e ) {
         // Re throw as something not stored in boost...
         throw RegError(e.what());
     }
 }
 
 RegPattern::~RegPattern() {
-    delete reinterpret_cast<boost::regex *>(pattern);
+    delete reinterpret_cast<regex *>(pattern);
 }
 
 bool RegPattern::Search(const std::string& haystack) const {
-    boost::regex& patternRegex = *( reinterpret_cast<boost::regex *>(pattern));
-    return boost::regex_search(haystack,patternRegex);
+    regex& patternRegex = *( reinterpret_cast<regex *>(pattern));
+    return regex_search(haystack,patternRegex);
 }
 
 std::string RegPattern::Group(const std::string& haystack, size_t n) const {
-    boost::regex& patternRegex = *( reinterpret_cast<boost::regex *>(pattern));
-    boost::smatch what;
+    regex& patternRegex = *( reinterpret_cast<regex *>(pattern));
+    smatch what;
 
     std::string match = "";
-    if ( boost::regex_search(haystack, what, patternRegex )) {
+    if ( regex_search(haystack, what, patternRegex )) {
         if ( n < what.size() ) {
             return what[n];
         }
@@ -39,8 +39,8 @@ std::string RegPattern::Group(const std::string& haystack, size_t n) const {
 std::string RegPattern::Replace(const std::string& haystack, 
                          const std::string& newNeedle) const
 {
-    boost::regex& patternRegex = *( reinterpret_cast<boost::regex *>(pattern));
-    return boost::regex_replace(haystack,patternRegex,
+    regex& patternRegex = *( reinterpret_cast<regex *>(pattern));
+    return regex_replace(haystack,patternRegex,
                                          newNeedle,
-                                         boost::match_default | boost::format_first_only);
+                                         regex_constants::match_default | regex_constants::format_first_only);
 }
